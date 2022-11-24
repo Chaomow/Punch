e64c6e66a364c9ccad4b27e270df3d2d2c7f5ab7
WayneChao <wayne415231@gmail.com>
2022-11-25 03:14:19 +0800

    Menu調整

--- app/projects/frontend/src/app/view/layout/layout.component.ts
+++ app/projects/frontend/src/app/view/layout/layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { environment } from '@environment';
 import { RouteService } from '@libs/service/route.service';
 import { MenuItem } from 'primeng/api';
@@ -12,46 +12,7 @@ import { MessageService } from 'primeng/api';
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
 })
-export class LayoutComponent implements OnInit {
+export class LayoutComponent {
   title = environment.title;
   menuItems: MenuItem[] = [];
-
-  /**
-   * constructor
-   *
-   * @param {MessageService} messageService 訊息
-   * @param {RouteServiceService} routeService 路由
-   */
-  constructor(
-    private messageService: MessageService,
-    private routeService: RouteService
-  ) {}
-
-  /**
-   * ngOnInit
-   */
-  ngOnInit() {
-    this.menuItems = [
-      {
-        label: '員工管理',
-        icon: 'pi pi-users',
-        routerLink: 'employee',
-      },
-      {
-        label: '登出',
-        icon: 'pi pi-sign-out',
-        /**
-         * command
-         */
-        command: () => {
-          this.routeService.go('login');
-          this.messageService.add({
-            severity: 'success',
-            summary: '登出',
-            detail: '成功',
-          });
-        },
-      },
-    ];
-  }
 }
--- app/projects/frontend/src/app/view/layout/menu/menu.component.ts
+++ app/projects/frontend/src/app/view/layout/menu/menu.component.ts
@@ -1,5 +1,6 @@
-import { Component, ElementRef, Input, ViewChild } from '@angular/core';
-import { MenuItem } from 'primeng/api';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { RouteService } from '@libs/service/route.service';
+import { MenuItem, MessageService } from 'primeng/api';
 
 /**
  * MenuComponent
@@ -9,15 +10,61 @@ import { MenuItem } from 'primeng/api';
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
   @ViewChild('menu') menu!: ElementRef<any>;
   @ViewChild('mask') mask!: ElementRef<any>;
-  @Input() items: MenuItem[] = [];
+  items: MenuItem[] = [];
+
+  /**
+   * constructor
+   *
+   * @param {MessageService} messageService 訊息
+   * @param {RouteServiceService} routeService 路由
+   */
+  constructor(
+    private messageService: MessageService,
+    private routeService: RouteService
+  ) {}
+
+  /**
+   * ngOnInit
+   */
+  ngOnInit() {
+    this.items = [
+      {
+        label: '員工管理',
+        icon: 'pi pi-users',
+        routerLink: 'employee',
+        /**
+         * command
+         */
+        command: () => {
+          this.hideMenu();
+        },
+      },
+      {
+        label: '登出',
+        icon: 'pi pi-sign-out',
+        /**
+         * command
+         */
+        command: () => {
+          this.hideMenu();
+          this.routeService.go('login');
+          this.messageService.add({
+            severity: 'success',
+            summary: '登出',
+            detail: '成功',
+          });
+        },
+      },
+    ];
+  }
 
   /**
    * 點擊漢堡條
    */
-  onIconClick() {
+  showMenu() {
     this.mask.nativeElement.classList.add('active');
     this.menu.nativeElement.classList.add('active');
   }
@@ -25,7 +72,7 @@ export class MenuComponent {
   /**
    * 點擊遮罩
    */
-  onMaskClick() {
+  hideMenu() {
     this.mask.nativeElement.classList.remove('active');
     this.menu.nativeElement.classList.remove('active');
   }
